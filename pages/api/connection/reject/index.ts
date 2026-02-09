92ede89e17af85c7e5feb90925b198daa0f82043
ilaydaufelek <ilaydaufelek@gmail.com>
2026-02-09 21:37:38 +0300

    rejected endpoint created

---
+++ pages/api/connection/reject/index.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { connectDB } from "@/lib/mongoose";
+import { Connection } from "@/db/models/connection";
+
+
+
+export async function POST(req: Request) {
+  await connectDB();
+
+  const { userId } = await auth();
+  if (!userId) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const { connectionId } = await req.json();
+
+  const connection = await Connection.findById(connectionId);
+
+  if (!connection) {
+    return NextResponse.json({ message: "Request not found" }, { status: 404 });
+  }
+
+  
+  if (connection.recipient.toString() !== userId) {
+    return NextResponse.json({ message: "Forbidden" }, { status: 403 });
+  }
+
+  await Connection.deleteOne({_id:connectionId});
+  return NextResponse.json({message:"Request rejected"})
+}
